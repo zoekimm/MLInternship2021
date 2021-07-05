d3e9c8592dc3be3e36d1ab3aea3ce71938ff8929
김민정 <minjeongkim@gimminjeong-ui-MacBook-Pro.local>
2021-07-05 13:39:34 +0900

    fix : for loop error

--- NLP/bert/bertCLS.py
+++ NLP/bert/bertCLS.py
@@ -1,3 +1,4 @@
+#keyword(1) similarity weight(all 1) tl_name
 import pickle
 from transformers import BertTokenizer, TFBertModel
 import numpy as np
@@ -8,23 +9,25 @@ import os
 from tempfile import mkdtemp
 import os.path as path
 
+
 class bert_CLS:
-    def __init__(self):
+    
+    def __init__(self, shape):
         self.tokenizer = self.get_tokenizer()
         self.model = self.get_model()
-        self.mat = self.get_empty_mat()
-        
+        self.mat = self.get_empty_mat(shape)
+
     def get_tokenizer(self):
         return BertTokenizer.from_pretrained("bert-base-uncased")
     
     def get_model(self):
         return TFBertModel.from_pretrained('bert-base-uncased')
     
-    def get_empty_mat(self): #use memmap to avoid memory issue
+    def get_empty_mat(self, shape):
         filename = path.join(mkdtemp(), 'word_list.dat')
         print(filename)
-        return np.memmap(filename, dtype='float32', mode='w+', shape=(36723, 768))
-    
+        return np.memmap(filename, dtype='float32', mode='w+', shape = shape) 
+        
     def tokenize(self, st):
         st = self.tokenizer.convert_tokens_to_ids(self.tokenizer.tokenize(st))
         st = [self.tokenizer.vocab[self.tokenizer.cls_token]] + st
@@ -33,22 +36,10 @@ class bert_CLS:
     
     def __call__(self, input_file):
         df = pd.read_csv(input_file, delimiter = "\n", names = ['st'])
-        index = 0
         for index, row in tqdm(df.iterrows(), total = df.shape[0]):
             st = row['st']
             st = self.tokenize(st) 
             y = self.model.predict(st)
             arr = np.expand_dims(np.squeeze(y[1]), axis = 0)
             self.mat[index] = arr[:]
-            index = index + 1
-
     
-    # generate model class 
-    model = bert_CLS()  
-    # execute model
-    em = model(input_file)
-    return em
-
-if __name__ == '__main__':
-    input_file = 'vocab.txt'
-    result = main(input_file)
