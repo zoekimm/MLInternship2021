76fe2e5def785ee25f6cfdad91f130b5113486f2
김민정 <minjeongkim@gimminjeong-ui-MacBook-Pro.local>
2021-07-05 13:43:44 +0900

    Create languagepreprocessor.py

---
+++ NLP/languagepreprocessor/languagepreprocessor.py
@@ -0,0 +1,30 @@
+import re
+import nltk
+import string
+import pickle
+from nltk.stem import WordNetLemmatizer 
+from nltk.tokenize import word_tokenize
+from konlpy.tag import Okt  
+from nltk.corpus import stopwords
+from normalise import normalise
+
+class LanguagePreprocessor():
+    
+    def __init__(self, lang, stop):
+        self.text = '' 
+        self.lang = lang
+        self.stop = stop
+
+    def get_text_eng(self, text):
+        regex = re.compile(f'[{string.punctuation+string.ascii_letters+string.whitespace}]+')
+        return ' '.join(regex.findall(text))
+    
+    def get_text_kor(self, text):
+        regex = re.compile(f'[^{string.punctuation+"ㄱ-힣"+string.whitespace}]+')
+        self.text = regex.sub(f'', self.text)
+        self.text = re.sub(r'(.)\1\1+',r'\1\1', self.text)
+        return text
+    
+    def normalize_text_eng(self):
+        self.text = ' '.join(normalise(word_tokenize(self.text), verbose=False))
+   
