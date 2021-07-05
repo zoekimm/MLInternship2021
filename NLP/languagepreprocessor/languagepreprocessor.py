c46ec204b8d21665dbaa922002f015cdaa8d8393
김민정 <zoek2100@gmail.com>
2021-07-06 07:58:35 +0900

    feat : added stopwords

--- NLP/languagepreprocessor/languagepreprocessor.py
+++ NLP/languagepreprocessor/languagepreprocessor.py
@@ -35,3 +35,27 @@ class LanguagePreprocessor():
     def stemming_kor(self):
         okt=Okt() 
         self.text = ' '.join(okt.nouns(self.text))
+         
+    def remove_stopwords_eng(self):
+        self.text = ' '.join([word.lower() for word in word_tokenize(self.text) if word not in set(stopwords.words('english'))])
+        
+    def remove_stopwords_kor(self):
+        if(self.stop != None):
+            open_file = open(self.stop, "rb")
+            stopwords = pickle.load(open_file)  
+            self.text = ' '.join([word.lower() for word in word_tokenize(self.text) if word not in set(stopwords)])
+        
+        
+    def __call__(self, text):     
+        if (self.lang == 'KOR'):
+            self.text = self.get_text_kor(text)
+            self.remove_stopwords_kor()
+            self.stemming_kor()
+        
+        if (self.lang == 'ENG'):
+            self.text = self.get_text_eng(text)
+            self.normalize_text_eng()
+            self.remove_stopwords_eng()
+            self.stemming_eng()
+
+        print(self.text)
