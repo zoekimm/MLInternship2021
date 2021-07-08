8d2d7d2defe60e3e82b16ad60b3e32fa32de2c39
김민정 <zoek2100@gmail.com>
2021-07-08 14:19:55 +0900

    test  : cifar10

--- scripts/model/resnet50.py
+++ scripts/model/resnet50.py
@@ -105,5 +105,23 @@ class ResNet50:
 
         return tf.keras.Model(inputs = input_t, outputs = x, name = 'ResNet50')
 
+    def __call__(self):
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+        x_train = x_train/255
+        x_test = x_test/255
+
+        #one-hot encoding
+        y_train = tf.keras.utils.to_categorical(y_train, 10)
+        y_test = tf.keras.utils.to_categorical(y_test, 10)
+
+        model = Resnet(input_shape = x_train.shape[1:], num_classes = 10)
+        model.compile(loss = 'categorical_crossentropy', optimizer = 'adam',metrics=['accuracy'])
+        model.fit(x_train, y_train, epochs = 1, batch_size = 16)
+        preds = model.evaluate(x_test, y_test)
+        print(preds[0], preds[1])
+        
+        return model
+        
     
         
