0127f8cf2a1491d6df27b320a36206ce5d8a63f0
김민정 <zoek2100@gmail.com>
2021-08-10 21:12:37 +0900

    feat : restore punctuations using fastpunct

---
+++ NLP/restore_punc.py
@@ -0,0 +1,33 @@
+import csv
+import spacy
+import pandas as pd
+from itertools import chain
+from fastpunct import FastPunct
+from punctuator import Punctuator
+
+class restore_punctuation:
+    
+    def __init__(self):
+        self.text_list = self.get_text_list()
+        self.d = self.get_empty_dict()
+        
+    def get_text_list(self):
+        f = open("punctuation_test.txt", "r")
+        reader = csv.reader(f)
+        text_list = [row for row in reader]
+        return list(chain(*text_list))
+        
+    def get_empty_dict(self):
+        return {'text': None, 'fastpunct': None, 'spacy': None, 'INTERSPEECH-T-BRNN.pcl(punctuator)': None, 'Demo-Europarl-EN.pcl(punctuator)': None}
+        
+    def fastpunct_punct(self, text):
+        fastpunct = FastPunct()
+        nlp=spacy.load('en_core_web_lg')
+
+        sent=[str(i) for i in nlp(text).sents]
+        
+        self.d['fastpunct'] = ' '.join(fastpunct.punct(sent, correct=False))
+
+   
+
+    
